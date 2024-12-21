2ce2a99b6d41d8572d81e94c0b835e6020d90043
AkechiShiro <14914796+AkechiShiro@users.noreply.github.com>
2024-12-21 08:37:09 +0100

    feat(input): move from unstable to codgician fork for Azure

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:codgician/nixpkgs/fix-azure-modules";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
